df7669c9c5616714728f2334bb4a715ddff7ed6e
school635-gushchina <43728887+school635-gushchina@users.noreply.github.com>
2020-04-09 13:44:57 +0300

    partial update

--- README.md
+++
@@ -1,2 +0,0 @@
-# school635-gushchina.github.io
-personal site of teacher of school 635
--- _vti_cnf/photos.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|12 Feb 2012 09:06:13 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_author:SR|Admin-PC\\Galina
-vti_modifiedby:SR|Admin-PC\\Galina
-vti_timecreated:TR|12 Feb 2012 09:06:13 -0000
-vti_cacheddtm:TX|12 Feb 2012 09:06:13 -0000
-vti_filesize:IR|6824
-vti_backlinkinfo:VX|
